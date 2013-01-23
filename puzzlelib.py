8bbdd4cd4d5e167cfdb48277b92fe816c166e6a7
nathan lachenmyer <asymptoticdesign@mit.edu>
2013-01-23 18:44:22 -0500

    first commit, anagram, transadd, transdelete finished

---
+++ puzzlelib.py
@@ -0,0 +1,87 @@
+"""
+Title: puzzle lib
+Author: nathan lachenmyer
+Description: A python library to manipulate hubway data stored on a server.
+Usage:
+Date Started: 2013 Jan
+Last Modified: 2013 Jan
+"""
+import sys
+from collections import Counter
+
+dictionary = []
+dict_file = open('/usr/share/dict/words','r')
+
+for word in dict_file:
+    dictionary.append(word.upper().strip("\n"))
+
+def anagram(pattern,dictionary=dictionary):
+    """
+    Find all anagrams of 'pattern' in the dictionary file
+    """
+    #turn word into a list of character
+    character_list = list(pattern.upper())
+    #sort the characters
+    character_list.sort()
+    #check against all words in our dictionary
+    for word in dictionary:
+        #check if words are the same length -- if not, they can't be anagrams of each other!
+        if(len(word) == len(character_list)):
+            sorted_word = sorted(word)
+            if character_list == sorted_word:
+                print word
+
+def superanagram(pattern,super_size,dictionary=dictionary):
+    """
+    Find all superanagrams of length n+1
+    """
+    #turn word into a list of character
+    character_list = list(pattern.upper())
+    #sort the characters
+    character_list = Counter(character_list)
+    #check against all words in our dictionary
+    for word in dictionary:
+        #check if words are the approprioate -- if not, they can't be anagrams of each other!
+        if(len(word) == len(character_list)+super_size):
+            if((character_list & Counter(word)) == character_list):
+                print word,
+
+def transadd(pattern,add_size=1,dictionary=dictionary):
+    """
+    Find all superanagrams of length n+1
+    Optional: select letter
+    """
+    #turn word into a list of character
+    character_list = list(pattern.upper())
+    #sort the characters
+    character_list = Counter(character_list)
+    #check against all words in our dictionary
+    for word in dictionary:
+        #check if words are the approprioate -- if not, they can't be anagrams of each other!
+        if(len(word) == len(character_list) + add_size):
+            wordCounter = Counter(word)
+            if((character_list & wordCounter) == character_list):
+                print word, (wordCounter - character_list).items()
+
+def transdelete(pattern,delete_size=1,dictionary=dictionary):
+    """
+    Find all superanagrams of length n+1
+    """
+    #turn word into a list of character
+    character_list = list(pattern.upper())
+    #sort the characters
+    character_list = Counter(character_list)
+    #check against all words in our dictionary
+    for word in dictionary:
+        #check if words are the approprioate -- if not, they can't be anagrams of each other!
+        if(len(word) + delete_size == len(character_list)):
+            wordCounter = Counter(word)
+            if((character_list & wordCounter) == wordCounter):
+                print word, (character_list-wordCounter).items()
+
+#to-do:
+#superanagram
+#subanagram
+#ceasar shift
+#words distance from
+#filter words
